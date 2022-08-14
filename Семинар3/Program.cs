00f00b2628ecbf39ca1ad89b79ada50919b8f0f9
Anna <Gera101184@gmail.com>
2022-08-14 19:40:40 +0300

    Семинар3четверти

---
+++ Семинар3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Console.WriteLine("Введите кординаты x ");
+// int x= Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите кординаты y ");ыы
+// int y= Convert.ToInt32(Console.ReadLine());
+// if (x>0 && y>0)
+// {
+//     Console.WriteLine ("Первая четверть");
+// }
+// else if (x>0 && y>0)
+// {
+//     Console.WriteLine ("Вторая четверть");
+// }
+// else if (x<0 && y<0)
+// {
+//     Console.WriteLine ("Третья четверть");
+// }
+// else if (x>0 && y<0)
+// {
+//     Console.WriteLine ("Четвертая четверть");
+// }
+// else
+// {
+//     Console.WriteLine ("Определить четверть не возможно");
+// }
